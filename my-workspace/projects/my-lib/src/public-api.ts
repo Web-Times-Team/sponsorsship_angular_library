9837336bc074fbc3fc9267b0307411782f27e489
Aremou <faizouaremou@gmail.com>
2020-12-01 19:57:14 +0100

    create menu bar tools

--- my-workspace/projects/my-lib/src/public-api.ts
+++ my-workspace/projects/my-lib/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of my-lib
  */
 
-export * from './lib/my-lib.service';
-export * from './lib/my-lib.component';
-export * from './lib/my-lib.module';
+export * from './lib/menu-bar/types/menu-bar-datas';
+export * from './lib/menu-bar/menu-bar/menu-bar.component';
+export * from './lib/menu-bar/menu-bar.module';
