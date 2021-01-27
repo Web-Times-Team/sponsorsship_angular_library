6338b394ec73860057eaf086c43dc4a81fb182da
faizou <faizouaremou@gmail.com>
2021-01-27 08:59:17 +0100

    improve menu-bar component

--- my-workspace/projects/angular-web-times-tools/src/lib/menu-bar/menu-bar/menu-bar.component.ts
+++ my-workspace/projects/angular-web-times-tools/src/lib/menu-bar/menu-bar/menu-bar.component.ts
@@ -2,6 +2,7 @@ import { Inject, Optional } from '@angular/core';
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuBarDatas, MENU_BAR_DATAS } from '../types/menu-bar-datas';
+import { NavLink } from '../types/nav-link';
 
 @Component({
   selector: 'wtt-menu-bar',
@@ -11,14 +12,26 @@ import { MenuBarDatas, MENU_BAR_DATAS } from '../types/menu-bar-datas';
 export class MenuBarComponent implements OnInit {
 
   @Input() menuBarDatas: MenuBarDatas
+  isDisplayedSubMenu: boolean;
   constructor(private router: Router, @Optional() @Inject(MENU_BAR_DATAS) config?: MenuBarDatas
   ) {
     if (MENU_BAR_DATAS) {
       this.menuBarDatas = config;
     }
+    this.isDisplayedSubMenu = false;
   }
 
   ngOnInit(): void {
   }
+
+  displaySubMenu(navLink: NavLink){
+    if (navLink.hasChild())
+    this.isDisplayedSubMenu = true;
+  }
+
+  hideSubMenu(navLink: NavLink){
+    if (navLink.hasChild())
+    this.isDisplayedSubMenu = false;
+  }  
 
 }
